d2915ce11e5c38d00819921c4823dc3e8a329fe3
Onetaway <onetaway@vip.qq.com>
2014-09-03 09:38:16 +0800

    First Commit

---
+++ My Cpp Way/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  My Cpp Way
+//
+//  Created by sanlen on 9/3/14.
+//  Copyright (c) 2014 Onetaway. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+##My Cpp Study Way
